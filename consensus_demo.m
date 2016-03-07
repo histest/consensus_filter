03592deb2915c72713598eb90abbcc6f62bb71ec
Colin Lu <landcolin@gmail.com>
2016-03-07 19:48:29 +0800

    error fix

--- consensus_demo.m
+++ consensus_demo.m
@@ -5,7 +5,7 @@ close all
 %% target
 % parameter initialization
 T = 0.1;
-nSteps = 100;
+nSteps = 200;
 nStates = 2;
 x_tgt_init = [3; 2];
 x_tgt = zeros(nSteps, nStates);
@@ -61,6 +61,10 @@ x_fltr = cell(nSteps, nSensors);
 P_fltr = cell(nSteps, nSensors);
 cm = [0 0 1; 1 0 0; 0 1 0];
 Q = Gamma * (Gamma)' * (sigma_v)^2;
+x_chnl_prev = cell(nSensors, nSensors);
+P_chnl_prev = cell(nSensors, nSensors);
+x_chnl_curr = cell(nSensors, nSensors);
+P_chnl_curr = cell(nSensors, nSensors);
 % main loop
 for i = 1 : nSteps
     % local Kalman filter
@@ -75,7 +79,7 @@ for i = 1 : nSteps
         [x_KF_pred, P_KF_pred] = KF_pred(x_KF_upd, P_KF_upd, F, Q);
         z = Z(i, j);
         [x_KF_upd, P_KF_upd] = KF_upd(z, x_KF_pred, P_KF_pred, H, R(j) );
-        % archive
+        % update
         x_fltr{i, j} = x_KF_upd;
         P_fltr{i, j} = P_KF_upd; 
     end
@@ -87,39 +91,58 @@ for i = 1 : nSteps
                 x_DKF = x_fltr{i, j};
                 P_DKF = P_fltr{i, j};
                 [x_DKF, P_DKF] = KF_upd(z, x_DKF, P_DKF, H, R(k) );
-                % archive
+                % update
                 x_fltr{i, j} = x_DKF;
                 P_fltr{i, j} = P_DKF;
             end
         end
     end
     % channel filter (consensus step 2)
-    nConsensus = 5;
+    nConsensus = 10;
     for j = 1 : nConsensus
-        x_fltr_backup = x_fltr;
-        P_fltr_backup = P_fltr;
+        % communication
         for k = 1 : nSensors
             for l = 1 : nSensors
                 if cm(l, k) == 1
-                    if i == 1
-                        x_p = F * x_fltr_init;
-                        P_p = F * P_fltr_init * F' + Q;
+                    x_chnl_prev{l, k} = x_chnl_curr{l, k};
+                    P_chnl_prev{l, k} = P_chnl_curr{l, k};
+                    x_chnl_curr{l, k} = x_fltr{i, l};
+                    P_chnl_curr{l, k} = P_fltr{i, l};
+                end
+            end 
+        end
+        % backup local estimate
+        x_fltr_backup = x_fltr;
+        P_fltr_backup = P_fltr;
+        % filter
+        for k = 1 : nSensors
+            x_1 = x_fltr_backup{i, k};
+            P_1 = P_fltr_backup{i, k};
+            for l = 1 : nSensors
+                if cm(l, k) == 1
+                    x_2 = x_chnl_curr{l, k};
+                    P_2 = P_chnl_curr{l, k};
+                    if j == 1
+                        if i == 1
+                            assert(isempty(x_chnl_prev{l, k}) )
+                            x_p = F * x_fltr_init;
+                            P_p = F * P_fltr_init * F' + Q;
+                        else
+                            x_p = F * x_chnl_prev{l, k};
+                            P_p = F * P_chnl_prev{l, k} * F' + Q;
+                        end
                     else
-                        x_p = F * x_fltr{i - 1, l};
-                        P_p = F * P_fltr{i - 1, l} * F' + Q;
+                        x_p = x_chnl_prev{l, k};
+                        P_p = P_chnl_prev{l, k};
                     end
-                    x_1 = x_fltr_backup{i, k};
-                    P_1 = P_fltr_backup{i, k};
-                    x_2 = x_fltr_backup{i, l};
-                    P_2 = P_fltr_backup{i, l};
                     [x_CF, P_CF] = chnl_fltr(x_p, P_p, x_1, P_1, x_2, P_2);
-                    % archive
+                    % update
                     x_fltr{i, k} = x_CF;
                     P_fltr{i, k} = P_CF;
                 end
             end
-        end
-    end
+        end       
+    end  
 end
 % testing
 figure
